e6df44046239765541e1d83106da0c15f4b687a9
wolfym1ng <305564381@qq.com>
2018-09-20 20:46:06 +0800

    my first php

---
+++ b.php
@@ -0,0 +1,47 @@
+<?php
+    /*$foo = 'bobo';
+    $bar = &$foo;
+    $bar = 'my name is';
+    echo $bar;
+    echo $foo;
+    $foo = 'he isssssss';
+    echo $bar;
+    echo $foo;*/
+    /*$foo = 34;
+    $bar = &$foo;
+    unset($bar);
+    echo $foo;*/
+    /*$bool = TRUE;
+    $str = 'foo';
+    $int = 123;
+    var_dump($bool);
+    var_dump($str);
+    var_dump($int);*/
+    /*echo 'this is a a  a a ';
+    echo 'thei is a \'simple\' string';
+    echo ' thies \ s s sdsed\s dsd s sdsd ds sdsd\\';*/
+    /*$beer = 'heineken';
+    echo "$beer's taste is greet\\n";
+    echo 'he drank some $beers\\n';
+    echo "he drank some ${beer}s\\n";
+    echo "he drand some {$beer}s\\n";*/
+    /*$name = 'my name';
+    echo <<<EOT
+    my name is $name,sldjfasjflsakjf/n.
+    \t djajfdafdsaf
+EOT;*/
+    $arr = array("foo" => "bar", 12 => 66);
+    print_r($arr);
+    echo "<br/>sdafasfd\\n<br/>";
+    echo $arr["foo"];
+    echo "<br/>$arr[12]";
+    /*class Person{
+        var $name;
+        function say(){
+            echo "stay hungry stay young";
+        }
+    }
+    $p = new Person;
+    $p->name = "ton";
+    $p->say();*/
+    ?>
